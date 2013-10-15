2aa50877d941e55d9b856298dcf231c682ee6dd8
Natuu <natyouu@gmail.com>
2013-10-15 18:35:29 +0200

    Finish the menu

--- js/nav.min.js
+++ js/nav.min.js
@@ -33,11 +33,6 @@ $(function() {
 	});
 
 
-	// Cette fonction permet de n'afficher que les entetes du menu
-
-	$('.li-menu').hide();
-	$('.li-menu:first-child').show();
-
 	// On affiche le sous-menu dans lequel on se trouve
 
 	var actualPage = '.submenu:contains("' + $('.section-title').text() + '")';
@@ -45,41 +40,6 @@ $(function() {
 	$(actualPage)
 	.addClass('submenu-actual')
 	.removeClass('submenu');
-
-	$('.submenu-actual > .li-menu').show();
-
-	// A chaque clic sur un sous menu, tout les li-menu sont cachés sauf l'entete
-
-	$('.submenu').click( function() {
-		$('.submenu > .li-menu').hide();
-		$('.submenu > .li-menu:first-child').show();
-	});
-
-	// Le sous menu qui a été cliqué afiche ses propres li-menu
-
-	$('.submenu:eq(0)').click( function() {
-		$('.submenu:eq(0) > .li-menu').show();
-	});
-
-	$('.submenu:eq(1)').click( function() {
-		$('.submenu:eq(1) > .li-menu').show();
-	});
-
-	$('.submenu:eq(2)').click( function() {
-		$('.submenu:eq(2) > .li-menu').show();
-	});
-
-	$('.submenu:eq(3)').click( function() {
-		$('.submenu:eq(3) > .li-menu').show();
-	});
-
-	$('.submenu:eq(4)').click( function() {
-		$('.submenu:eq(4) > .li-menu').show();
-	});
-
-	$('.submenu:eq(5)').click( function() {
-		$('.submenu:eq(5) > .li-menu').show();
-	});
 
 
 
